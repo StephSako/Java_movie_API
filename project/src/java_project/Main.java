0185dd3ab37947cb474f17dc56c1e414295f6e4f
Steph Sako <stephen.sakovitch@orange.fr>
2019-04-30 13:58:25 +0200

    FIxing ERROR (dsl g dékoné avc le stash wola)

--- project/src/java_project/Main.java
+++ project/src/java_project/Main.java
@@ -5,10 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         RechercheFilm r = new RechercheFilm("bdd/bdfilm.sqlite");
-<<<<<<< Updated upstream
-        System.out.println(r.retrouve("DE John"));
-=======
         System.out.println(r.retrouve("DE niro"));
->>>>>>> Stashed changes
     }
 }
