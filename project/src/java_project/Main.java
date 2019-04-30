8f3e4592f4e2241af04d74ec9689f9e02978cdf5
Steph Sako <stephen.sakovitch@orange.fr>
2019-04-30 14:06:57 +0200

    Ajout .gitignore

--- project/src/java_project/Main.java
+++ project/src/java_project/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         RechercheFilm r = new RechercheFilm("bdd/bdfilm.sqlite");
-        System.out.println(r.retrouve("DE niro"));
+        System.out.println(r.retrouve("EN 2009"));
     }
 }
